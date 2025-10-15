e0d7e77da5704dc58dca9027b9e6d08014f8c12c
Clément Pickel <clement.pickel@epitech.eu>
2025-10-15 15:29:48 +0200

    add top cause and top donators from recu fiscal

--- UI/TopCause.py
+++ UI/TopCause.py
@@ -4,29 +4,32 @@ from dateutil.relativedelta import relativedelta
 import pandas as pd
 from pandas.tseries.offsets import DateOffset
 
-def settings_top_donator():
+def settings_cause():
     st.subheader("Paramètres")
     start = st.date_input(
         "Début",
-        value=date.today() - relativedelta(years=1),
+        value=date.today() - relativedelta(years=5),
         min_value=date(2000, 1, 1),
         max_value=date.today(),
-        key=0
+        key=4
     )
     end = st.date_input(
         "Fin",
         value=date.today(),
         min_value=date(2000, 1, 1),
         max_value=date.today(),
-        key=1
+        key=5
     )
-    limit = st.number_input("Limite", value=10, min_value=1, step=1)
-    return start, end, limit
+    hide = st.checkbox("Cacher le CD0000")
+    return start, end, hide
 
-def display_top_donators(df):
-    st.subheader("💰 Top donateurs")
-    st.dataframe(df)
+def display_top_cause(df: pd.DataFrame, hide):
+    if hide:
+        df = df[df["CAUSE_NAME"] != "CD0000-NATIONAL"]
 
-
-
-
+    st.subheader("🌊Top Cause par Don")
+    st.bar_chart(
+        data=df,
+        x="CAUSE_NAME",
+        y="TOTAL_DONS",
+    )
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit as st
-from Service.MarketingService import MarketingService, cache_get_reglement_regulier, cache_get_top_donateurs, cache_get_top_donateurs_regulier
+from Service.MarketingService import MarketingService, cache_get_reglement_regulier, cache_get_top_donateurs, cache_get_top_donateurs_regulier, cache_get_top_cause
 from UI.BarChartTotalReglementPonctuel import display_bar_chart_ponctuel, select_settings_bar_chart_ponctuel
 from UI.TopDonators import display_top_donators, settings_top_donator
 from UI.ReglementRegulier import display_reglement_regulier, settings_reglement_regulier
+from UI.TopCause import settings_cause, display_top_cause
 from Model.interval import Interval
 import numpy as np
 from datetime import date
@@ -18,11 +19,11 @@ with col2:
     df = cache_get_reglement_regulier(interval, start, end, agg)
     display_bar_chart_ponctuel(interval, df)
     
-col1, col2 = st.columns([9, 1])
-with col2:
-    start, end, limit = settings_top_donator()
+col1, col2 = st.columns([1, 9])
 with col1:
-    top_donateur_df = cache_get_top_donateurs(start, end, limit)
+    start, end, limit = settings_top_donator()
+with col2:
+    top_donateur_df = cache_get_top_donateurs(limit)
     display_top_donators(top_donateur_df)
 
 
@@ -33,3 +34,10 @@ with col1:
 with col2:
     top_donateur_reg_df = cache_get_top_donateurs_regulier(start, end, 10000)
     display_reglement_regulier(top_donateur_reg_df, start, end)
+
+col1, col2 = st.columns([1, 9])
+with col1:
+    start, end, hide = settings_cause()
+with col2:
+    top_cause_df = cache_get_top_cause(start, end)
+    display_top_cause(top_cause_df, hide)
